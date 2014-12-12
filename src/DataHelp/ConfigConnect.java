0838a8ea2a26abe4ea6f966053a159fe84531f50
bangquanhung <bangquanhung@10.211.55.4>
2014-12-12 14:46:42 +0700

    first pro

---
+++ src/DataHelp/ConfigConnect.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DataHelp;
+
+import com.oracle.jrockit.jfr.ContentType;
+import java.sql.Connection;
+
+/**
+ *
+ * @author bangquanhung
+ */
+public class ConfigConnect {
+    private String host="";
+    private String user="";
+    private String pass="";
+    //method get connection voi mysql
+    public Connection getCon(String host, String user, String pass){
+        Connection cn = null;
+        try{
+            //neu la mysql
+            Class.forName(com)
+        }
+        catch(Exception ex){
+            ex.getMessage();
+        }
+        return cn;
+    }
+   
+    
+    
+}
---
+++ src/DataHelp/DataHelp.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DataHelp;
+
+import java.sql.Connection;
+
+/**
+ *
+ * @author bangquanhung
+ */
+public class DataHelp {
+    
+    private String host="";
+    private String user="";
+    private String pass="";
+    
+}
